0907f5392fce1ab9696aff57da6548c7c515b467
fujing <fuijng54426@163.com>
2014-08-06 10:19:20 +0800

    修改页面结构copyright

--- app/views/format/copyright.php
+++ app/views/format/copyright.php
@@ -1,18 +1,18 @@
 <!--=== Copyright ===-->
-<div class="copyright">
+<!--<div class="copyright">-->
     <div class="container">
-        <div class="row">
-            <div class="col-md-6">
-                <p class="copyright-space">
-                    2013 &copy; Unify. ALL Rights Reserved.
-                    <a href="#">Privacy Policy</a> | <a href="#">Terms of Service</a>
+        <div class="row margin-top-100">
+            <div class="col-md-12">
+                <p class="text-center">
+                     网球通俱乐部   京ICP备12008858号
+                    <a href="#">网球通</a> | <a href="#">gotennis</a>
                 </p>
             </div>
-            <div class="col-md-6">
-                <a href="index.html">
-                    <img id="logo-footer" src="/assets/img/logo2-default.png" class="pull-right" alt="" />
-                </a>
-            </div>
+<!--            <div class="col-md-6">-->
+<!--                <a href="index.html">-->
+<!--                    <img id="logo-footer" src="/assets/img/logo.png" class="pull-right" alt="" />-->
+<!--                </a>-->
+<!--            </div>-->
         </div><!--/row-->
     </div><!--/container-->
-</div><!--/copyright-->	
+<!--</div><!--/copyright-->
