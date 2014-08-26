b66f4b9f99e289b1fe1de82074f2fb0a6855ac80
xiongmeng <277309623@qq.com>
2014-08-26 17:15:37 +0800

    调整工作台布局即增加提示信息

--- app/views/instantOrder/order_court_manage.php
+++ app/views/instantOrder/order_court_manage.php
@@ -1,7 +1,7 @@
-<div class="container ">
-    <div class="row margin-bottom-20">
-        <div class="tab-v1 col-xs-12 col-md-12">
-            <ul class="nav nav-tabs">
+<div class="container worktable" id="worktable">
+    <div class="row halls">
+        <div class="tab-v2 col-xs-12 col-md-12">
+            <ul class="nav nav-tabs" style="border-bottom: solid 1px #dddddd">
                 <?php foreach ($halls as $hall) { ?>
                     <li role="presentation" class="<?php if ($hall->id == $hallID) { ?>active<?php } ?>"><a
                             href="/order_court_manage?hall_id=<?php echo $hall->id ?>&court_id=">
@@ -11,25 +11,31 @@
         </div>
     </div>
 
-    <div class="row">
-        <div class="tab-v3 col-md-2">
-            <ul class="nav nav-pills nav-stacked">
+    <div class="row margin-bottom-10">
+        <div class="tab-v1 col-xs-12 col-md-10 dates">
+            <ul class="nav nav-tabs">
                 <?php foreach ($dates as $date => $time) { ?>
                     <li class="<?php if ($date == $activeDate) { ?>active<?php } ?>">
                         <a href="/order_court_manage?hall_id=<?= $hallID ?>&date=<?= $date ?>">
                             <?= date('m-d', $time) ?>&nbsp;<?= $weekdayOption[date('w', $time)]; ?>
                         </a>
                     </li>
-
                 <?php } ?>
             </ul>
         </div>
-        <div class="table-responsive pin-container col-md-10" id="table_court">
-            <div class="worktable-toolbar pinned row">
-                <a class="btn btn-danger btn-lg" data-bind="click: batchOnline, css:{disabled: currentState()!='draft'}">上架</a>
-                <a class="btn btn-danger btn-lg" data-bind="click: batchOffline, css:{disabled: currentState()!='on_sale'}">下架</a>
-            </div>
+    </div>
 
+    <div class="row">
+        <div class="toolbar pinned col-md-12" data-bind="visible: selected().length>0" style="display: none">
+            <a class="btn btn-danger btn-lg"
+               data-bind="click: batchOnline, visible: currentState()=='draft'">上架</a>
+            <a class="btn btn-danger btn-lg"
+               data-bind="click: batchOffline, visible: currentState()=='on_sale'">下架</a>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="table-responsive pin-container col-md-12" id="table_court">
             <?php if (empty($instantOrders)) { ?>
                 <div class="alert alert-info"><strong>没有可出售的场地！</strong></div>
             <?php } else { ?>
@@ -49,6 +55,7 @@
                                     <?php $instantOrder = $formattedInstants[$court->id][$startHour]; ?>
                                     <!-- ko with:$root.instantOrders[<?= $court->id ?>][<?= $startHour ?>]-->
                                     <a name="<?= 'instant-order-' . $court->id . '-' . $startHour ?>"
+                                       title="<?= $startHour.'-'.($startHour+1). ' ' . $court->number . '号场'?>"
                                     <?php if ($instantOrder->state == 'draft') { ?>
                                         class="instant-order online"
                                               data-bind="click: $root.select, css: {active: select}">&nbsp;
@@ -80,9 +87,9 @@
 
 <script>
     seajs.use('court/manage', function (courtManage) {
-        courtManage.init($('#table_court')[0],
+        courtManage.init($('#worktable')[0],
             <?= json_encode(array('instantOrders' => $formattedInstants))?>);
-        $(".pinned").pin({'containerSelector': '.pin-container', padding: {top: 5}});
+//        $(".pinned").pin({'containerSelector': '.pin-container', padding: {top: 5}});
 
         $('.countDown').kkcountdown({callback: function () {
             window.location.reload()
