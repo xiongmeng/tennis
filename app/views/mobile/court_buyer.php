8cee98b2dd2285a14502494261850ef20f75e6f1
xiongmeng <277309623@qq.com>
2014-08-28 22:21:45 +0800

    手机端引入alert，测试modal好不好使

--- app/views/mobile/court_buyer.php
+++ app/views/mobile/court_buyer.php
@@ -14,12 +14,30 @@
     <!-- Include the compiled Ratchet CSS -->
     <link href="/mobile/css/ratchet.css" rel="stylesheet">
 
-    <link href="/mobile/css/app.css" rel="stylesheet">
+    <link href="/mobile/css/app.css?1" rel="stylesheet">
+    <link rel="stylesheet" href="/assets/plugins/bootstrap/css/bootstrap.css">
 
     <!-- Include the compiled Ratchet JS -->
     <script src="/mobile/js/ratchet.js"></script>
 
+    <script type="text/javascript" src="/assets/plugins/seajs/sea.js"></script>
+    <script type="text/javascript" src="/assets/js/seajs-config.js"></script>
     <script type="text/javascript" src="/assets/plugins/jquery-1.10.2.min.js"></script>
+    <script type="text/javascript" src="/assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+
+    <script type="text/javascript">
+        seajs.config({
+            paths: {
+                plugin: '/assets/plugins',
+                module: '/assets/js/module'
+            },
+            base: '/assets/js/module/',
+            charset: 'utf-8',
+            map: [
+                [/(.*\/assets\/js\/module\/.*\.(?:css|js|tpl))(?:.*)$/i, '$1?' + '201408221157']
+            ]
+        });
+    </script>
 </head>
 <body>
 
@@ -27,91 +45,102 @@
     <h1 class="title push-left">123</h1>
 </header>
 
-<!-- Wrap all non-bar HTML in the .content div (this is actually what scrolls) -->
-<div class="content">
-    <div class="segmented-control">
+<!-- Block button in standard bar fixed below top bar -->
+<div class="bar bar-standard bar-header-secondary">
+    <div class="segmented-control worktable">
         <?php foreach ($dates as $date => $time) { ?>
-            <a class="control-item <?php if ($date == $activeDate) { ?>active<?php } ?>"
-               href="/mobile_court_buyer/<?= $hallID ?>?date=<?= $date ?>">
+            <a class="date <?php if ($date == $activeDate) { ?>active<?php } ?>"
+               href="/mobile_court_buyer/<?= $hallID ?>?date=<?= $date ?>" data-ignore="push">
                 <?= date('m-d', $time) ?>
             </a>
         <?php } ?>
     </div>
+</div>
 
-    <div class="col-md-12 table-responsive pin-container" id="table_court">
-        <?php if (empty($instantOrders)) { ?>
-            <div class="alert alert-info"><strong>没有可出售的场地！</strong></div>
-        <?php } else { ?>
-            <div class="table-court">
-                <div class="col-hour">
-                    <a class="hour disabled">&nbsp;</a>
-                    <?php for ($startHour = $instantOrders->first()->start_hour; $startHour < $instantOrders->last()->start_hour; $startHour++) { ?>
-                        <a class="hour disabled"
-                           name="hour-<?= $startHour ?>"><?= $startHour, '-' . ($startHour + 1) ?></a>
-                    <?php } ?>
-                </div>
-                <?php foreach ($courts as $court) { ?>
-                    <div class="col-instant-order">
-                        <a name="court-<?= $court->id ?>" class="court disabled"><?= $court->number ?>号场</a>
-                        <?php for ($startHour = $instantOrders->first()->start_hour; $startHour < $instantOrders->last()->start_hour; $startHour++) { ?>
-                            <?php if (isset($formattedInstants[$court->id]) && isset($formattedInstants[$court->id][$startHour])) { ?>
-                                <?php $instantOrder = $formattedInstants[$court->id][$startHour]; ?>
-                                <!-- ko with:$root.instantOrders[<?= $court->id ?>][<?= $startHour ?>]-->
-                                <a name="<?= 'instant-order-' . $court->id . '-' . $startHour ?>"
-                                   title="<?= $startHour.'-'.($startHour+1). ' ' . $court->number . '号场'?>"
-                                    <?php if ($instantOrder->state == 'on_sale') { ?>
-                                   class="instant-order buy"
-                                   data-bind="click: $root.select, css: {active: select}">
-                                    <span style="font-size: small" class="money">￥</span><span
-                                        data-bind="text: quote_price"></span>
-
-                                    <?php } else if ($loginUserId == $instantOrder->buyer && $instantOrder->state == 'paying') { ?>
-
-                                        class="instant-order paying" data-bind="click: $root.select,
-                                        css: {active: select}">
-                                        <span style="font-size: small" class="money">￥</span><span
-                                            data-bind="text: quote_price"></span>
-                                        <em style="color: red" class="countDown" data-bind="attr: {'data-time': parseInt(expire_time())+60}"></em>
-
-                                    <?php } else if ($loginUserId == $instantOrder->buyer && $instantOrder->state == 'payed') { ?>
-                                        class="instant-order living" style="background-color: #f0ad4e">待打球
-                                    <?php } else { ?>
-                                        class="instant-order">&nbsp;
-                                    <?php } ?>
-                                </a>
-                                <!--/ko-->
-                            <?php } else { ?>
-                                <span class="instant-order">&nbsp;</span>
-                            <?php } ?>
-                        <?php } ?>
-                    </div>>
+<div class="content">
+    <?php if (empty($instantOrders)) { ?>
+        <div class="alert alert-info"><strong>没有可出售的场地！</strong></div>
+    <?php } else { ?>
+        <div class="table-court">
+            <div class="col-hour">
+                <a class="hour disabled">&nbsp;</a>
+                <?php for ($startHour = $instantOrders->first()->start_hour; $startHour < $instantOrders->last()->start_hour; $startHour++) { ?>
+                    <a class="hour disabled"
+                       name="hour-<?= $startHour ?>"><?= $startHour, '-' . ($startHour + 1) ?></a>
                 <?php } ?>
             </div>
-        <?php } ?>
+            <?php foreach ($courts as $court) { ?>
+                <div class="col-instant-order">
+                    <a name="court-<?= $court->id ?>" class="court disabled"><?= $court->number ?>号场</a>
+                    <?php for ($startHour = $instantOrders->first()->start_hour; $startHour < $instantOrders->last()->start_hour; $startHour++) { ?>
+                        <?php if (isset($formattedInstants[$court->id]) && isset($formattedInstants[$court->id][$startHour])) { ?>
+                            <?php $instantOrder = $formattedInstants[$court->id][$startHour]; ?>
+                            <!-- ko with:$root.instantOrders[<?= $court->id ?>][<?= $startHour ?>]-->
+                            <a name="<?= 'instant-order-' . $court->id . '-' . $startHour ?>"
+                               title="<?= $startHour.'-'.($startHour+1). ' ' . $court->number . '号场'?>"
+                                <?php if ($instantOrder->state == 'on_sale') { ?>
+                               class="instant-order buy"
+                               data-bind="click: $root.select, css: {active: select}">
+                                <span style="font-size: small" class="money">￥</span><span
+                                    data-bind="text: quote_price"></span>
+
+                                <?php } else if ($loginUserId == $instantOrder->buyer && $instantOrder->state == 'paying') { ?>
+
+                                    class="instant-order paying" data-bind="click: $root.select,
+                                    css: {active: select}">
+                                    <span style="font-size: small" class="money">￥</span><span
+                                        data-bind="text: quote_price"></span>
+                                    <em style="color: red" class="countDown" data-bind="attr: {'data-time': parseInt(expire_time())+60}"></em>
+
+                                <?php } else if ($loginUserId == $instantOrder->buyer && $instantOrder->state == 'payed') { ?>
+                                    class="instant-order living" style="background-color: #f0ad4e">待打球
+                                <?php } else { ?>
+                                    class="instant-order">&nbsp;
+                                <?php } ?>
+                            </a>
+                            <!--/ko-->
+                        <?php } else { ?>
+                            <span class="instant-order">&nbsp;</span>
+                        <?php } ?>
+                    <?php } ?>
+                </div>>
+            <?php } ?>
+        </div>
+    <?php } ?>
+</div>
+
+<!-- Block button in standard bar fixed above the footer -->
+<div class="bar bar-standard bar-footer-secondary" data-bind="visible: selected().length>0">
+    <div class="segmented-control">
+        <a class="control-item "
+           data-bind="click: batchBuy, visible: currentState()=='on_sale'">buy</a>
+        <a class="control-item "
+           data-bind="click: batchPay, visible: currentState()=='paying'">pay</a>
+        <a class="control-item "
+           data-bind="click: batchCancelBuy, visible: currentState()=='paying'">cancel-buy</a>
     </div>
 </div>
 
+<!--==nav==-->
+<nav class="bar bar-tab bar-footer">
+    <a class="tab-item" href="<?= url_wrapper('/mobile_home/instant') ?>" data-transition="slide-in"
+       data-ignore="push">
+home    </a>
+    <a class="tab-item" href="<?= url_wrapper('/mobile_buyer') ?>" data-transition="slide-in" data-ignore="push">
 
-    <!--==nav==-->
-    <nav class="bar bar-tab">
-        <a class="tab-item" href="<?= url_wrapper('/mobile_home/instant') ?>" data-transition="slide-in"
-           data-ignore="push">
-            <span class="icon icon-home"></span>
-            <span class="tab-label">首页</span>
-        </a>
-        <a class="tab-item" href="<?= url_wrapper('/mobile_buyer') ?>" data-transition="slide-in" data-ignore="push">
-            <span class="icon icon-person"></span>
+center
+    </a>
+    <a class="tab-item" href="http://homestead.app:8000/order_court_buyer/8935" data-transition="slide-in" data-ignore="push">
 
-            <span class="tab-label">个人中心</span>
-
-        </a>
-        <a class="tab-item" href="<?= url_wrapper('#r') ?>" data-transition="slide-in" data-ignore="push">
-
-            <span class="icon icon-star-filled"></span>
-            <span class="tab-label">提醒</span>
-        </a>
-    </nav>
-    <!--==end nav==-->
-
+notice    </a>
+</nav>
+<!--==end nav==-->
+<script>
+    seajs.use('/mobile/js/manage', function (courtManage) {
+        courtManage.init($('#worktable')[0],
+            <?= json_encode(array('instantOrders' => $formattedInstants))?>);
+    })
+    ;
+</script>
 </body>
 </html>
---
+++ public/mobile/js/manage.js
@@ -0,0 +1,131 @@
+define(function(require){
+    var ko = require('knockout');
+    var mapping = require('knockout_mapping');
+    require('knockout_switch_case');
+    require('rest');
+    require('bootbox');
+    require('kkcountdown');
+
+    var option = {
+        'submitUrl' : ''
+    };
+
+    var ListModel = function(mappingModel){
+        var self = mappingModel;
+        self.selected = ko.observableArray();
+        self.currentState = ko.observable('');
+
+        self.selected.subscribe(function(newSelected){
+            var length = newSelected.length;
+            if(length <= 0){
+                self.currentState('');
+            }else{
+                var lastInstantOrder = newSelected[length-1];
+                self.currentState(lastInstantOrder.state());
+            }
+        });
+
+        self.select = function(instantOrder, event){
+            if(instantOrder.select()){
+                instantOrder.select(false);
+                self.selected.remove(instantOrder);
+            }else{
+                if(self.currentState() == '' || self.currentState() == instantOrder.state()){
+                    instantOrder.select(true);
+                    self.selected.push(instantOrder);
+                }
+            }
+        };
+
+        function doBatch(operate){
+            var selectedIds = [];
+            $.each(self.selected(), function(index, instantOrder){
+                selectedIds.push(instantOrder.id());
+            });
+            var defer = $.restPost('/instantOrder/batchOperate',
+                {'operate' : operate, 'instant_order_ids' : selectedIds.join(',')});
+
+            defer.done(function(res, data){
+                window.location.reload();
+            });
+            defer.fail(function(){
+
+            });
+        }
+
+        self.batchOnline= function(){
+            doBatch('online');
+        };
+        self.batchOffline = function(){
+            doBatch('offline');
+        };
+        self.batchCancelBuy = function(){
+            doBatch('cancel_buy');
+        };
+
+        function doPay(url){
+            var selectedIds = [];
+            var selectedMoney = 0;
+
+            $.each(self.selected(), function(index, instantOrder){
+                selectedIds.push(instantOrder.id());
+                selectedMoney += parseInt(instantOrder.quote_price());
+            });
+
+            var confirmText = "共选取<mark>" + selectedIds.length + "</mark>个时段，共计<mark>" + selectedMoney + "</mark>元";
+            bootbox.confirm(confirmText, function(result){
+                if(!result){
+                    return ;
+                }
+                var defer = $.restPost(url, {'instant_order_ids' : selectedIds.join(',')});
+
+                defer.done(function(res, data){
+
+                    if(data.status == 'no_money'){
+                        $modalDom = $('#dialog-go-to-pay');
+                        $model = mapping.fromJS(data);
+                        ko.applyBindings($model, $modalDom[0]);
+
+                        $modalDom.modal().on('hidden.bs.modal', function () {
+                            window.location.reload();
+                        });
+                    }else if(data.status == 'pay_success'){
+                        bootbox.alert('恭喜您，购买成功！您可以在已购买订单里面查看详情！').on('hidden.bs.modal', function () {
+                            window.location.reload();
+                        });
+                    }
+                });
+            });
+        }
+
+        self.batchBuy = function(){
+            doPay('/instantOrder/batchBuy');
+        };
+        self.batchPay = function(){
+            doPay('/instantOrder/batchPay');
+        };
+
+        return self;
+    };
+
+    function init(dom, worktableData, cfg){
+        $.extend(option, cfg);
+
+        var list = new ListModel(mapping.fromJS(worktableData));
+        ko.applyBindings(list, dom);
+
+//        var defer = $.restGet('/xm/instantOrder/view/8935/2014-08-18/');
+//
+//        defer.done(function(res, data){
+//            if(!list){
+//                list = new ListModel(mapping.fromJS(data));
+//            }else{
+//                mapping.fromJS(data, list);
+//            }
+//        });
+    }
+
+    return {
+        init : init
+    }
+});
