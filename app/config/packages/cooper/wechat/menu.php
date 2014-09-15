7b0bf28556b97debb712fdb2bfaf852408c86d02
fujing <fuijng54426@163.com>
2014-09-15 11:57:43 +0800

    微信测试新自定义菜单

--- app/config/packages/cooper/wechat/menu.php
+++ app/config/packages/cooper/wechat/menu.php
@@ -4,48 +4,21 @@ return array(
 
      "button":[
      {
-           "name":"订场服务",
-           "sub_button":[
-           {
-               "type":"click",
-               "name":"推荐场馆",
-               "key":"Recommend_Court"
+           "type":"click",
+           "name":"预订场地",
+           "key":"Reserve",
 
-            },
-            {
-               "type":"click",
-               "name":"附近场馆",
-               "key":"Nearby_Court"
-            },
-            {
-               "type":"click",
-               "name":"常订场馆",
-               "key":"Ordered_Court"
-            },
-            {
-               "type":"click",
-               "name":"搜索场馆",
-               "key":"Search_Court"
-            }]
        },
        {
-           "name":"会员中心",
-           "sub_button":[
-           {
-               "type":"click",
-               "name":"注册/绑定",
-               "key":"Add_Bond"
-            },
-            {
-               "type":"click",
-               "name":"会员服务",
-               "key":"Member_Sever"
-            }]
+           "type":"click",
+           "name":"即时订场",
+           "key":"Instant"
+
        },
      {
           "type":"click",
-          "name":"即时订场",
-          "key":"Instant_Order"
+          "name":"活动资讯",
+          "key":"Active"
       }]
 
  }'
