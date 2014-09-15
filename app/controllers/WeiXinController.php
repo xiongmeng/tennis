fec2e4359a4b2d522b886612e89e332a72db014b
fujing <fuijng54426@163.com>
2014-09-15 12:36:00 +0800

    微信测试新自定义菜单

--- app/controllers/WeiXinController.php
+++ app/controllers/WeiXinController.php
@@ -55,7 +55,7 @@ class WeiXinController extends \BaseController
 
                // $key = $message['key']; //获取当前菜单key
                 $isBond = $this->getUser($appUserID);
-                $res = WeChatMsg($appUserID,$isBond);
+                $res = $this->WeChatMsg($appUserID,$isBond);
                     $reply = $server->getXml4RichMsgByArray($res);
                     echo $reply;
 
@@ -108,6 +108,90 @@ class WeiXinController extends \BaseController
         return $app;
 
     }
+
+    function WeChatMsg($appUserID,$isBond){
+        if($isBond){
+            return array(
+                'WeChatMsg'=> array(
+                    '0' => array(
+                        'title' => '订场就找网球通',
+                        'desc' => '订场就找网球通',
+                        'pic' => "http://www.wangqiuer.com/assets/img/logo.jpg",
+                        'url' => "http://www.gotennis.cnbond?app_user_id=" . $appUserID . '&app_id=2'
+                    ),
+                    '1' => array(
+                        'title' => '预约订场',
+                        'desc' => '预定场地1',
+                        'pic' => "http://www.wangqiuer.com/assets/img/logo.jpg",
+                        'url' => "http://www.gotennis.cn/reserve/recommend?app_user_id=" . $appUserID . '&app_id=2'
+                    ),
+                    '2' => array(
+                        'title' => '即时订场',
+                        'desc' => '预定场地1',
+                        'pic' => "http://www.wangqiuer.com/assets/img/logo.jpg",
+                        'url' => "http://www.gotennis.cn/reserve/recommend?app_user_id=" . $appUserID . '&app_id=2'
+                    ),
+                    '3' => array(
+                        'title' => '活动资讯',
+                        'desc' => '预定场地1',
+                        'pic' => "http://www.wangqiuer.com/assets/img/logo.jpg",
+                        'url' => "http://www.gotennis.cn/reserve/recommend?app_user_id=" . $appUserID . '&app_id=2'
+                    ),
+                    '4' => array(
+                        'title' => '个人中心',
+                        'desc' => '预定场地1',
+                        'pic' => "http://www.wangqiuer.com/assets/img/logo.jpg",
+                        'url' => "http://www.gotennis.cn/reserve/recommend?app_user_id=" . $appUserID . '&app_id=2'
+                    ),
+                )
+            );
+        }
+        else{
+            return array(
+                'WeChatMsg'=> array(
+                    '0' => array(
+                        'title' => '订场就找网球通',
+                        'desc' => '订场就找网球通',
+                        'pic' => "http://www.wangqiuer.com/assets/img/logo.jpg",
+                        'url' => "http://www.gotennis.cnbond?app_user_id=" . $appUserID . '&app_id=2'
+                    ),
+                    '1' => array(
+                        'title' => '预约订场',
+                        'desc' => '预定场地1',
+                        'pic' => "http://www.wangqiuer.com/assets/img/logo.jpg",
+                        'url' => "http://www.gotennis.cn/reserve/recommend?app_user_id=" . $appUserID . '&app_id=2'
+                    ),
+                    '2' => array(
+                        'title' => '即时订场',
+                        'desc' => '预定场地1',
+                        'pic' => "http://www.wangqiuer.com/assets/img/logo.jpg",
+                        'url' => "http://www.gotennis.cn/reserve/recommend?app_user_id=" . $appUserID . '&app_id=2'
+                    ),
+                    '3' => array(
+                        'title' => '活动资讯',
+                        'desc' => '预定场地1',
+                        'pic' => "http://www.wangqiuer.com/assets/img/logo.jpg",
+                        'url' => "http://www.gotennis.cn/reserve/recommend?app_user_id=" . $appUserID . '&app_id=2'
+                    ),
+                    '4' => array(
+                        'title' => '绑定用户',
+                        'desc' => '预定场地1',
+                        'pic' => "http://www.wangqiuer.com/assets/img/logo.jpg",
+                        'url' => "http://www.gotennis.cn/reserve/recommend?app_user_id=" . $appUserID . '&app_id=2'
+                    ),
+                    '5' => array(
+                        'title' => '注册用户',
+                        'desc' => '预定场地1',
+                        'pic' => "http://www.wangqiuer.com/assets/img/logo.jpg",
+                        'url' => "http://www.gotennis.cn/reserve/recommend?app_user_id=" . $appUserID . '&app_id=2'
+                    ),
+                )
+            );
+        }
+
+
+    }
+
 
     /**
      * save Log to SQL
