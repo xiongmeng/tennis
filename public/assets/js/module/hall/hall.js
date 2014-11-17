b5c2c08b83e5a45572027eba0a5e675a551ef77d
xiongmeng <277309623@qq.com>
2014-11-17 14:35:49 +0800

    场馆图像js model中增加首页字段

--- public/assets/js/module/hall/hall.js
+++ public/assets/js/module/hall/hall.js
@@ -135,6 +135,7 @@ define(function (require) {
         ];
         self.hours = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24];
 
+        self.image = ko.observable(hallData.image);
         self.images = ko.observable();
         self.images.plupload_cfg = {
             url: '/hall/saveImage/' + hallData.id,
