416d797029287eade6faacc4327f5a677e4ac9b6
xiongmeng <277309623@qq.com>
2014-11-26 12:25:57 +0800

    调整约球报名确认页面的样式

--- app/views/js_css_header.php
+++ app/views/js_css_header.php
@@ -10,7 +10,7 @@
 
 <!-- Include the compiled Ratchet CSS -->
 <link href="/mobile/css/ratchet.css" rel="stylesheet">
-<link href="/mobile/css/app.css" rel="stylesheet">
+<link href="/mobile/css/app.css?201411261211" rel="stylesheet">
 
 <!-- Include the compiled Ratchet JS -->
 <!--<script src="/mobile/js/ratchet.js"></script>-->
--- app/views/mobile/seeking_join.php
+++ app/views/mobile/seeking_join.php
@@ -1,20 +1,39 @@
+<style>
+    .list-detail {
+        color: #777
+    }
+
+    .list-detail li{
+        padding-right: 12px
+    }
+
+    .list-detail .name {
+        width: 30%;
+        float: left
+    }
+
+    .list-detail .value {
+        width: 70%;
+        float: left;
+        word-wrap: break-word
+    }
+</style>
 <div class="content" style="margin-bottom: 50px; padding-top: 26px">
-    <ul class="table-view" style="color: #777">
-        <li class="table-view-cell" style="padding-right: 12px">
-            <label style="width: 25%;float: left">报名人员：</label>
+    <ul class="table-view list-detail">
+        <li class="table-view-cell">
+            <label class="name">报名人员：</label>
             <?php $wxUserProfile = cache_weChat_profile($user->user_id); ?>
-            <label
-                style="width: 75%; float: left"><?= sprintf("%s（%s）", $user->nickname, !empty($wxUserProfile) ? $wxUserProfile->nickname : '') ?></label>
+            <label class="value"><?= sprintf("%s（%s）", $user->nickname, !empty($wxUserProfile) ? $wxUserProfile->nickname : '') ?></label>
         </li>
         <li class="table-view-cell" style="padding-right: 12px">
-            <label style="width: 25%;float: left">约球信息：</label>
-            <label style="width: 75%; float: left"><?= seeking_brief($seeking, '') ?></label>
+            <label class="name">约球信息：</label>
+            <label class="value"><?= seeking_brief($seeking, '') ?></label>
         </li>
         <li class="table-view-cell">
-            <label>当前余额：<?= $balance ?>元</label>
+            <label class="name">当前余额：</label><label class="value"><?= $balance ?>元</label>
         </li>
         <li class="table-view-cell">
-            <label>需要花费：<?= $seeking->personal_cost ?>元</label>
+            <label class="name">需要花费：</label><label class="value"><?= $seeking->personal_cost ?>元</label>
         </li>
     </ul>
 
