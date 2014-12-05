924a4671fcbee7f177edd41d0cec92321ad2b15d
xiongmeng <277309623@qq.com>
2014-12-05 10:09:35 +0800

    微信场馆页调整成分页模式

--- app/views/hall/wx/list.php
+++ app/views/hall/wx/list.php
@@ -1,3 +1,5 @@
+
+
 <div class="bar bar-nav tab" style="border: 0">
     <div class="segmented-control worktable" data-bind="foreach:tabs" style="padding-top: 5px; border: 0">
         <a class="date" data-bind="css:{active:$root.curTab() == id}, click:$root.selectTab, text:name"
@@ -6,49 +8,79 @@
     </div>
 </div>
 
-<div class="content" style="margin-bottom: 50px;">
+<header class="bar bar-standard bar-header-secondary" data-bind="visible:curTab()=='search',with:queries" style="display: none; border: 0">
+    <form style="display: inline" class="search" action="/mobile_home/reserve/recommend">
+        <input style="width: 65%;font-size:15px;"
+               type="search" class="pull-left" placeholder="请输入场馆名" data-bind="value:name">
+        <input type="submit" class="btn btn-primary" data-bind="click: $root.search" value="search">
+    </form>
+</header>
 
-    <ul class="table-view hall-on-sale">
-        <!--ko foreach: cacheData -->
-
+<div class="content" style="margin-bottom: 50px;" data-bind="style:{'padding-top': curTab()!='search' ? '40px' :'80px'}">
+    <ul class="table-view hall-on-sale" data-bind="visible:total()<1 && !inSearching()" style="display: none">
+        <li class="notice">
+            <p data-bind="visible:curTab()=='nearby'">您还没有同意上传地理位置信息哦！</p>
+            <p data-bind="visible:curTab()=='history'">您还没有预订过场地哦！</p>
+            <p data-bind="visible:curTab()=='search'">木有找见合适的场馆哦！</p>
+        </li>
+    </ul>
+    <ul class="table-view hall-on-sale" data-bind="foreach: data, visible:total()>0" style="display: none">
         <li class="table-view-cell media">
-            <a data-bind="click:$root.next">
+            <a style="padding: 10px" data-bind="attr:{href:'/hall_reserve?hall_id=' + id()}" data-ignore="push">
                 <img class="media-object pull-left head-img" data-bind="attr:{src:head_wx_url}">
-
                 <div class="media-body description" style="width: 70%; float: left">
                     <p class="name" data-bind="text:name"></p>
-
                     <p><span class="header">地址：</span><span data-bind="text:area_text"></span></p>
-
                     <p><span class="header">电话：</span><span data-bind="text:telephone"></span></p>
                 </div>
             </a>
+            <a style="padding: 10px" data-bind="attr:{href:'/hall_reserve?hall_id=' + id()}" data-ignore="push">
+                <table>
+                    <thead>
+                    <tr>
+                        <th>时段</th>
+                        <th>门市价</th>
+                        <th>普通会员</th>
+                        <th>金卡会员</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tbody data-bind="foreach: hall_prices">
+                    <tr data-bind="if:id()">
+                        <td data-bind="text:name"></td>
+                        <td data-bind="text:market"></td>
+                        <td data-bind="text:member"></td>
+                        <td class="vip" data-bind="text:vip"></td>
+                    </tr>
+                    </tbody>
+                </table>
+            </a>
         </li>
-
-        <!--/ko-->
     </ul>
 </div>
 
+<!-- Block button in standard bar fixed above the footer -->
+<div class="bar bar-standard bar-footer-secondary toolbar">
+    <div class="segmented-control" data-bind="visible:inSearching()" style="display: none">
+        <p class="control-item" >正在加载， 请稍候...</p>
+    </div>
+
+    <div class="segmented-control" data-bind="visible:!inSearching() && last_page()>current_page()" style="display: none">
+        <a class="control-item" data-bind="click:$root.pre">上一页</a>
+        <span class="control-item"><span data-bind="text:current_page"></span>/<span data-bind="text:last_page"></span>页</span>
+        <a class="control-item" data-bind="click:$root.next">下一页</a>
+    </div>
+</div>
 
 <script type="text/javascript">
     seajs.use(['hall/list', 'knockout_switch_case'], function (HallList, switchCase) {
-        var hallList = new HallList({model: 'falls', relations: 'HallPrices'});
+        var hallList = new HallList({model: 'page', relations: 'HallPrices'});
 
         var recommendTab = {
             id: 'recommend',
             name: '推荐场馆',
             cfg: {url: '/hall/active/list/<?= HALL_ACTIVE_RECOMMEND?>'}
         };
-
-        var searchTab = {
-            id: 'search',
-            name: '搜索场馆',
-            cfg: {
-                url: '/hall/search',
-                per_page: 4
-            }
-        };
-
         hallList.tabs = [
             recommendTab,
             {
@@ -56,7 +88,14 @@
                 name: '附近场馆',
                 cfg: {url: '/hall/nearby'}
             },
-            searchTab,
+            {
+                id: 'search',
+                name: '搜索场馆',
+                cfg: {
+                    url: '/hall/search',
+                    per_page: 20
+                }
+            },
             {
                 id: 'history',
                 name: '常订场馆',
@@ -72,17 +111,15 @@
             tab.cfg && (hallList.cfg = $.extend(hallList.cfg, tab.cfg));
 
             hallList.data.removeAll();
-//            if (tab.id != 'search') {
+            if(tab.id != 'search'){
                 hallList.search();
-//            }
+            }
         };
 
         hallList.queries.stat(<?= HALL_STAT_PUBlISH?>);
 
-
         ko.applyBindings(hallList, $('#body')[0]);
 
-        hallList.selectTab(searchTab);
-
+        hallList.selectTab(recommendTab);
     });
 </script>
